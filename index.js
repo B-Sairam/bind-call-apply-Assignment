e57202bd4e2fc70348b935c143e978bc0b5a94a4
B-Sairam <87373136+B-Sairam@users.noreply.github.com>
2021-08-14 17:29:37 +0530

    bind,call,apply () assignment
    ..

---
+++ index.js
@@ -0,0 +1,23 @@
+// call() ||  apply() || bind() in javascrpit
+var user = {
+    name:'Hulk',
+    age:18,
+    getName : function( a ){
+        console.log('user name is ' +this.name +" "+a);
+    },
+}
+
+var user2 = {
+    name:'Iron man'
+}
+//==============bind() method======================
+
+let x = user.getName.bind(user2);
+x("super_Hero");
+
+//===============call() method=====================
+user.getName.call(user2,'super_Hero');
+
+//===============apply() method====================
+
+user.getName.apply(user2,['super_Hero'])
